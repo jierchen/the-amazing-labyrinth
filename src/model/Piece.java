e0476a35db0cf79ad752e95ded9d8ff38b8660c3
Jier Chen <cje9507@gmail.com>
2021-09-17 21:10:46 -0400

    Add abstract piece model to define game piece board location

---
+++ src/model/Piece.java
@@ -0,0 +1,36 @@
+package model;
+
+public abstract class Piece {
+
+    // row and column positions
+    private int row;
+    private int col;
+
+    /**
+     * Constructor
+     *
+     * @param row row position on the board
+     * @param col column position on the board
+     */
+    public Piece(int row, int col) {
+        // set positions
+        this.row = row;
+        this.col = col;
+    }
+
+    // Getters and setters
+    public int getRow() {
+        return this.row;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+}
