bc9b277a3b2c6a170603fd51cc92d64314b70ac6
Jier Chen <cje9507@gmail.com>
2021-09-17 21:03:34 -0400

    Add treasure model class

---
+++ src/model/Treasure.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Treasure {
+
+    public static final int TREASURE_AMOUNT = 24;
+
+    private int treasureNum;
+    private boolean collected = false;
+
+    /**
+     * Constructor
+     *
+     * @param treasureNum id of this treasure
+     */
+    public Treasure(int treasureNum) {
+        this.treasureNum = treasureNum;
+    }
+
+    // Setters and getters
+    public int getTreasureNum() {
+        return this.treasureNum;
+    }
+
+    public void setCollected(boolean collected) {
+        this.collected = collected;
+    }
+
+    public boolean isCollected() {
+        return this.collected;
+    }
+
+}
