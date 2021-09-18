9c455e016777c6ad7979071ff2e0165b17f74a4a
Jier Chen <cje9507@gmail.com>
2021-09-17 21:13:33 -0400

    Add player model skeleton class

---
+++ src/model/Player.java
@@ -0,0 +1,84 @@
+package model;
+
+import model.tiles.Tile;
+
+import java.util.Stack;
+
+public class Player extends Piece {
+
+    private String colour;
+    private Stack<Card> hand;
+
+    // Home tile information
+    private Tile homeTile;
+    private boolean returnedHome = false;
+
+    /**
+     * Constructor
+     * @param row starting row on the board
+     * @param col starting column on the board
+     * @param colour player colour identifier
+     */
+    public Player(int row, int col, String colour) {
+        super(row, col);
+
+        this.colour = colour;
+    }
+
+    /**
+     * Draws the next card for player
+     */
+    public void goToNextCard() {
+
+    }
+
+    /**
+     * sets the top card of hand to this new card
+     * @param card The card to be sent to the top of the stack
+     */
+    public void addToHand(Card card){
+
+    }
+
+    /**
+     * Gets the top card from the hand stack
+     *
+     * @return the top card from hand or null if hand is empty
+     */
+    public Card getTopOfHand() {
+
+    }
+
+    /**
+     * Checks if the player has won by checking if player still has cards left
+     * @return if the player has won
+     */
+    public boolean hasCollectedAll() {
+
+    }
+
+    // Getters and setters
+    public void setHand(Stack<Card> hand) {
+        this.hand = hand;
+    }
+
+    public Stack<Card> getHand() {
+        return this.hand;
+    }
+
+    public void setReturnedHome(boolean returnedHome) {
+        this.returnedHome = returnedHome;
+    }
+
+    public boolean hasReturnedHome() {
+        return this.returnedHome;
+    }
+
+    public void setHomeTile(Tile homeTile) {
+        this.homeTile = homeTile;
+    }
+
+    public Tile getHomeTile() {
+        return this.homeTile;
+    }
+}
