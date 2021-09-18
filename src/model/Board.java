bc6f7537fe14fdb695e7905f83ea299b7150d7db
Jier Chen <cje9507@gmail.com>
2021-09-18 01:09:50 -0400

    Implement game and board setup

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,6 +1,13 @@
 package model;
 
+import model.tiles.ITile;
+import model.tiles.LTile;
+import model.tiles.TTile;
 import model.tiles.Tile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Board {
 
@@ -26,6 +33,33 @@ public class Board {
             {'0',' ','0',' ','0',' ','3'},
     };
 
+    /**
+     * Helper class to encapsulate tile {@code type} and {@code treasureNum} pair
+     */
+    private static class TypeAndTreasureNum {
+
+        private char type;
+        private int treasureNum;
+
+        public TypeAndTreasureNum (char type, int treasureNum) {
+            this.type = type;
+            this.treasureNum = treasureNum;
+        }
+
+        // getters
+        public char getType() {
+            return this.type;
+        }
+
+        public int getTreasureNum() {
+            return this.treasureNum;
+        }
+
+        public String toString(){
+            return String.valueOf(type);
+        }
+    }
+
     // Board tiles
     // Order is left to right -> top to bottom
     private Tile[][] tiles = new Tile[NUM_OF_TILES_PER_SIDE][NUM_OF_TILES_PER_SIDE];
@@ -48,15 +82,88 @@ public class Board {
 
     /**
      * Initializes the board by setting up the board tiles
+     *
+     * @return the leftover insertable tile
      */
-    public void init() {
+    public Tile init() {
 
+        Tile insertableTile = setupTiles();
+        connectTiles(0, this.tiles.length - 1, 0, this.tiles.length - 1);
+        connectPlayersToTiles();
+
+        return insertableTile;
     }
 
     /**
      * Initializes the board tiles properties
+     *
+     * @return returns the extra, insertable board tile
      */
-    private void setupTiles() {
+    private Tile setupTiles() {
+        final int MAX_SHIFTABLE_TREASURE = 6;
+        final int STATIONARY_TILE_AMOUNT = 12;
+
+        // Initial tile data generation
+        ArrayList<TypeAndTreasureNum> shiftableTilesData = new ArrayList<TypeAndTreasureNum>();
+        int shiftableTreasureCounter = STATIONARY_TILE_AMOUNT;
+
+        for(int i = 0; i < TTile.TILE_AMOUNT; i++) {
+            shiftableTilesData.add(new TypeAndTreasureNum('T', (i < MAX_SHIFTABLE_TREASURE ? shiftableTreasureCounter : -1)));
+            shiftableTreasureCounter++;
+        }
+
+        for(int i = 0; i < LTile.TILE_AMOUNT; i++) {
+            shiftableTilesData.add(new TypeAndTreasureNum('L', (i < MAX_SHIFTABLE_TREASURE ? shiftableTreasureCounter : -1)));
+            shiftableTreasureCounter++;
+        }
+
+        for(int i = 0; i < ITile.TILE_AMOUNT; i++) {
+            shiftableTilesData.add(new TypeAndTreasureNum('I', -1));
+        }
+
+        // Randomize shiftable tiles data
+        Collections.shuffle(shiftableTilesData, new Random());
+        System.out.println(shiftableTilesData);
+
+        // Setup board tiles
+        int stationaryTreasureCounter = 0;
+        int dataCounter = 0;
+
+        for(int row = 0; row < this.tiles.length; row++) {
+            for(int col = 0; col < this.tiles[row].length; col++) {
+
+                char tileType = STATIONARY_TILES_TYPE_MATRIX[row][col];
+
+                // set-up preset/stationary tiles
+                if(tileType != ' ') {
+
+                    // Stationary L-tiles have no treasures
+                    if(tileType == 'L') {
+                        this.tiles[row][col] = generateTile(row, col, tileType, -1, true);
+                    }
+                    else {
+                        this.tiles[row][col] = generateTile(row, col, tileType, stationaryTreasureCounter, true);
+                        stationaryTreasureCounter++;
+                    }
+                }
+                // setup movable tiles
+                else {
+                    TypeAndTreasureNum currentTileData = shiftableTilesData.get(dataCounter);
+
+                    this.tiles[row][col] = generateTile(row, col, currentTileData.getType(), currentTileData.getTreasureNum(),false);
+                    dataCounter++;
+                }
+            }
+        }
+
+        // Setup extraTile
+        TypeAndTreasureNum extraTileData = shiftableTilesData.get(dataCounter);
+
+        // create extra tile object
+        Tile insertableTile = generateTile(-1, -1, extraTileData.getType(), extraTileData.getTreasureNum(), false);
+        insertableTile.setInsertable(true);
+
+        return insertableTile;
 
     }
 
@@ -72,7 +179,32 @@ public class Board {
      * @return the appropriate {@code Tile}
      */
     private Tile generateTile(int row, int col, char tileType, int treasureNum, boolean isStationary) {
+        Tile newTile;
+        int orientation;
+        Random rand = new Random();
 
+        // generate orientation
+        if(isStationary) {
+            orientation = (STATIONARY_TILES_ORIENTATION_MATRIX[row][col] - '0');
+        } else {
+            orientation = (rand.nextInt(4));
+        }
+
+        // generate new tile object
+        if(tileType == 'T') {
+            newTile = new TTile(row, col,  orientation);
+        } else if (tileType == 'L') {
+            newTile = new LTile(row, col,  orientation);
+        } else {
+            newTile = new ITile(row, col, orientation);
+        }
+
+        //add treasure
+        if(treasureNum != -1) {
+            newTile.setTreasure(this.treasures[treasureNum]);
+        }
+
+        return newTile;
     }
 
     /**
@@ -84,14 +216,48 @@ public class Board {
      * @param colEnd end of column constraint
      */
     private void connectTiles(int rowStart, int rowEnd, int colStart, int colEnd) {
+        // Limit the bounds
+        rowStart = Math.max(rowStart, 0);
+        rowEnd = Math.min(rowEnd, this.tiles.length - 1);
+        colStart = Math.max(colStart, 0);
+        colEnd = Math.min(colEnd, this.tiles.length - 1);
 
+        for(int row = rowStart; row <= rowEnd; row++) {
+            for(int col = colStart; col <= colEnd; col++) {
+
+                // Connects to the tile above
+                if(row != 0 && this.tiles[row][col].getOpening(0) && this.tiles[row - 1][col].getOpening(2)) {
+                    this.tiles[row][col].addAdjTile(this.tiles[row - 1][col]);
+                }
+
+                // Connects to the tile to the right
+                if(col != 6 && this.tiles[row][col].getOpening(1) && this.tiles[row][col + 1].getOpening(3)) {
+                    this.tiles[row][col].addAdjTile(this.tiles[row][col + 1]);
+                }
+
+                // Connects to the tile below
+                if(row != 6 && this.tiles[row][col].getOpening(2) && this.tiles[row + 1][col].getOpening(0)) {
+                    this.tiles[row][col].addAdjTile(this.tiles[row + 1][col]);
+                }
+
+                // Connects to the tile to the left
+                if(col != 0 && this.tiles[row][col].getOpening(3) && this.tiles[row][col - 1].getOpening(1)) {
+                    this.tiles[row][col].addAdjTile(this.tiles[row][col - 1]);
+                }
+            }
+        }
     }
 
     /**
      * Adds players to the tiles they're on
      */
     private void connectPlayersToTiles() {
+        for(Player player: players) {
+            Tile homeTile = this.tiles[player.getRow()][player.getCol()];
 
+            player.setHomeTile(homeTile);
+            homeTile.addPlayerOnTile(player);
+        }
     }
 
     /**
--- src/model/Game.java
+++ src/model/Game.java
@@ -2,12 +2,18 @@ package model;
 
 import model.tiles.Tile;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class Game {
 
     public static final Integer NUM_PLAYERS = 4;
 
     // Game board
     private Board board;
+
+    // Game cards to distribute to players
+    private Card[] cards;
 
     // Treasures on the board
     private Treasure[] treasures;
@@ -32,28 +38,76 @@ public class Game {
      * and game states of all pieces
      */
     public void init() {
+        // Setup pre-game components
+        this.setupCardsAndTreasures();
+        this.distributeCards();
 
+        // Initialize new board
+        this.board = new Board(this.players, this.treasures);
+        this.insertableTile = this.board.init();
     }
 
     /**
      * Initializes players for the game
      */
     private void setupPlayers() {
+        // Preset player data
+        String[] colours =  {"Red", "Blue", "Green", "Purple"};
+        int[][] startingPoints = {{0,0}, {6, 0}, {0, 6}, {6, 6}};
 
+        for(int playerNum = 0; playerNum < NUM_PLAYERS; playerNum++) {
+            this.players[playerNum] = new Player(startingPoints[playerNum][0],
+                    startingPoints[playerNum][1], colours[playerNum]);
+        }
     }
 
     /**
      * Initializes treasures and treasure cards
      */
     private void setupCardsAndTreasures() {
+        // Create treasures and treasure cards
+        this.treasures = new Treasure[Treasure.TREASURE_AMOUNT];
+        this.cards = new Card[Treasure.TREASURE_AMOUNT];
 
+        for(int i = 0; i < Treasure.TREASURE_AMOUNT; i++) {
+            this.treasures[i] = new Treasure(i);
+            this.cards[i] = new Card(this.treasures[i]);
+        }
     }
 
     /**
      * Shuffles and distributes 4 cards from the pool to each player
      */
     private void distributeCards() {
+        Card[] shuffledCards = new Card[cards.length];
 
+        System.arraycopy(cards, 0, shuffledCards, 0, cards.length);
+
+        Random rand = new Random();
+
+        // Fisher-Yates shuffle
+        for(int i = shuffledCards.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+
+            // swap
+            Card temp = shuffledCards[index];
+            shuffledCards[index] = shuffledCards[i];
+            shuffledCards[i] = temp;
+        }
+
+        int cardsPerPlayer = 5;
+        int cardCounter = 0;
+
+        for(Player player: players) {
+            Stack<Card> newHand = new Stack<Card>();
+
+            for(int j = 0; j < cardsPerPlayer; j++) {
+                newHand.push(shuffledCards[cardCounter]);
+                cardCounter++;
+            }
+
+            player.setHand(newHand);
+        }
     }
 
     /**
