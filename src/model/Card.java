498e25c89c50ac46e58ab04fd38d0a050317875d
Jier Chen <cje9507@gmail.com>
2021-09-17 21:12:13 -0400

    Add card model class

---
+++ src/model/Card.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Card {
+
+    // Treasure to collect for this card
+    private Treasure treasure;
+
+    /**
+     * Constructor
+     *
+     * @param treasure indicated treasure to collect
+     */
+    public Card(Treasure treasure) {
+        this.treasure = treasure;
+    }
+
+    // Setters and getters
+    public Treasure getTreasure() {
+        return this.treasure;
+    }
+
+}
