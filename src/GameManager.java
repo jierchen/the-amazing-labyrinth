ee867f0f92b4b29eda900e348060586f906c2a83
Jier Chen <cje9507@gmail.com>
2021-09-18 20:12:22 -0400

    Remove GameManager's TurnState inner class

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,58 +1,12 @@
 import controller.GameController;
 import model.Game;
 import model.Player;
+import util.TurnState;
 import view.GameDisplay;
 
 public class GameManager {
 
     public static final int NUM_PLAYERS = 4;
-
-    /**
-     * Enscapulation class to hold current turn data to enforce rules
-     */
-    public class TurnState {
-        private int playerTurn;
-        private boolean insertedTile;
-        private boolean moved;
-        private boolean aiTurn;
-        private boolean gameEnded = false;
-
-        private TurnState(int playerTurn, boolean insertedTile, boolean moved, boolean aiTurn) {
-            this.playerTurn = playerTurn;
-            this.insertedTile = insertedTile;
-            this.moved = moved;
-            this.aiTurn = aiTurn;
-        }
-
-        // Getters and Setters
-        public void setPlayerTurn(int playerTurn) {
-            this.playerTurn = playerTurn;
-        }
-        public int getPlayerTurn() {
-            return playerTurn;
-        }
-
-        public void setInsertedTile(boolean insertedTile) {
-            this.insertedTile = insertedTile;
-        }
-        public boolean hasInsertedTile() {
-            return insertedTile;
-        }
-
-        public void setMoved(boolean moved) {
-            this.moved = moved;
-        }
-        public boolean hasMoved() {
-            return moved;
-        }
-
-        public void setGameEnded(boolean gameEnded) {
-            this.gameEnded = gameEnded;
-        }
-        public boolean hasGameEnded() {
-            return gameEnded;
-        }
-    }
 
     private GameController gameController;
     private GameDisplay gameDisplay;
@@ -86,6 +40,6 @@ public class GameManager {
         GameDisplay gameDisplay = new GameDisplay(game);
 
         // Setup game controller
-        GameController gameController = new GameController(gameDisplay, game);
+        GameController gameController = new GameController(turnState, gameDisplay, game);
     }
 }
