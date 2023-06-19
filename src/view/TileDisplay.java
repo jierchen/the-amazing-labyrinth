b45def2a8c8737166e6cd0a4d2a491ceffc6aa74
Jier Chen <j793chen@uwaterloo.ca>
2023-06-18 23:35:41 -0400

    Fix TileDisplay by adding tile image and treasure image

--- src/view/TileDisplay.java
+++ src/view/TileDisplay.java
@@ -25,11 +25,13 @@ public class TileDisplay extends JLayeredPane {
     private void setTreasureImageProperties() {
         treasureImage.setSize(35,35);
         treasureImage.setLocation(25,27);
+        add(treasureImage);
     }
 
     private void setTileImageProperties() {
         tileImage.setSize(TILE_SIDE_LENGTH, TILE_SIDE_LENGTH);
         tileImage.setLocation(0, 0);
+        add(tileImage);
     }
 
     private void setTileDisplayProperties() {
